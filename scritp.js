f402a77c4e45da8ba95c5f7830a61cc267454477
francisco-hue <58644424+francisco-hue@users.noreply.github.com>
2024-11-26 09:39:46 -0300

    Add files via upload

---
+++ scritp.js
@@ -0,0 +1,5 @@
+// Função para mostrar a mensagem de mais detalhes sobre a promoção
+function mostrarMensagem() {
+    const mensagemPromo = document.getElementById('mensagemPromo');
+    mensagemPromo.textContent = 'Aproveite essa promoção exclusiva para os primeiros clientes! Comprando 3 caixas, você leva uma de presente. Só até o final do mês!';
+}
